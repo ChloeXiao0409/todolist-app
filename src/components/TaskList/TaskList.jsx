ccca4395e9634b972c9e5186f2688207ce627529
Chloe-Xiao <ChloeXiao0409@outlook.com>
2025-02-11 17:07:04 +1100

    task-list-add

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,6 +1,28 @@
-// import Task from "../Task/Task";
-// import "./TaskList.scss";
+import Task from "../Task/Task";
+import "./TaskList.scss";
 
+const TaskList = ({tasks, setTasks}) => {
+    // tasks -> objects -> [{id: 1, text: "Task 1", completed: false}]
+    // setTasks -> function to state updating, comes from useState in App.js
+    const completeTask = (id) => {
+        setTasks(tasks.map(task => 
+            task.id === id ? {...task, completed: !task.completed} : task
+        ));
+    } 
 
+    const deleteTask = (id) => {
+        setTasks(tasks.filter(task => task.id !== id));
+    }    
 
-// export default TaskList;
+    return (
+        <div className="task-list">
+            {
+                tasks.map(task => (
+                    <Task key={task.id} task={task} onComplete={completeTask} onDelete={deleteTask} />
+                ))
+            }
+        </div>
+    )
+}
+
+export default TaskList;
