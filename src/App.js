4c932d527f74a04e35e8312f4349a679fd70ebcd
Chloe-Xiao <ChloeXiao0409@outlook.com>
2025-02-15 16:51:00 +1100

    Update-App&global-sass

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, use } from "react";
+import TaskList from "./components/TaskList/TaskList";
 import './App.scss';
 
-function App() {
+const App = () => {
+  // tasks -> objects store in the Array -> [{id: 1, text: "Task 1", completed: false}]
+  const [tasks, setTasks] = useState([]);
+  // newTask -> string to store the new task
+  const [newTask, setNewTask] = useState("");
+
+  // Load tasks from localStorage when the app starts
+  useEffect(() => {
+    const savedTasks = JSON.parse(localStorage.getItem("tasks"));
+    if (savedTasks) return setTasks(savedTasks);
+  }, []);
+  // Save tasks to localStorage when the tasks state changes
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  // AddTask -> function to add new task to the tasks state
+  const addTask = () => {
+    if (newTask.trim() === "") return;
+    setTasks([...tasks, { id: Date.now(), text: newTask, completed: false}]);
+    setNewTask("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>To-do List App</h2>
+      <input className="task-input" value={newTask} onChange={(e) => setNewTask(e.target.value)}></input>
+      <button className="add-btn" onClick={addTask}>➕ Add Task</button>
+      <TaskList tasks={tasks} setTasks={setTasks} />
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -3,10 +3,10 @@ import "./Task.scss";
 const Task = ({ task, onComplete, onDelete}) => {
     return (
         <div className="task">
+            <input className="completed-checkbox" type="checkbox" onClick={() => onComplete(task.id)} />
             <span className={`task-text ${task.completed ? "completed" : ""}`}>
                 {task.text}
             </span>
-            <button className="completed-btn" onClick={() => onComplete(task.id)}>✅</button>
             <button className="deleted-btn" onClick={() => onDelete(task.id)}>❌</button>
         </div>
     );
